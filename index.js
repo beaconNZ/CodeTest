6c40edc6aa17ef408ff36e7be083d4a4d41bc1da
Chambo88 <benchamberlain88@gmail.com>
2022-02-09 17:22:18 +1300

    added csv and args functionality

--- index.js
+++ index.js
@@ -1,28 +1,89 @@
-var myArgs = process.argv.slice(2);
-console.log(romanToInt(myArgs[0]));
-function romanToInt(s) {
-    var romanMap = new Map([
-        ['I', 1,],
-        ['V', 5,],
-        ['X', 10,],
-        ['L', 50,],
-        ['C', 100,],
-        ['D', 500,],
-        ['M', 1000,],
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var csv_parse_1 = require("csv-parse");
+var findTrafficSolutionFromCSV = function (fileLocaiton) {
+    var csvData = [];
+    fs.createReadStream(fileLocaiton)
+        .pipe((0, csv_parse_1.parse)({ delimiter: ":" }))
+        .on("data", function (csvrow) {
+        csvData.push(csvrow[0].split(","));
+    })
+        .on("end", function () {
+        csvData.forEach(function (roadEntry) {
+            var roads = roadEntry.map(function (numString) {
+                return parseInt(numString, 10);
+            });
+            findTrafficSolution(roads);
+        });
+    });
+};
+findTrafficSolutionFromCSV("./data.csv");
+var findTrafficSolutionWithArgs = function () {
+    var myArgs = process.argv.slice(2);
+    var roads = myArgs.map(function (numString) {
+        return parseInt(numString, 10);
+    });
+    findTrafficSolution(roads);
+};
+function findTrafficSolution(roads) {
+    var roundaboutMap = new Map([
+        ["low", 0.9],
+        ["medium", 0.75],
+        ["high", 0.5],
     ]);
-    var total = 0;
-    var prevNum = null;
-    var toAdd = null;
-    for (var i = 0; i < s.length; i++) {
-        var value = romanMap.get(s[i]);
-        if (value != null) {
-            if (prevNum != null && prevNum < value) {
-                total -= prevNum * 2;
-            }
-            total += value;
-            prevNum = value;
-        }
+    var trafficLightMap = new Map([
+        ["low", 0.3],
+        ["medium", 0.75],
+        ["high", 0.9],
+    ]);
+    var stopSignMap = new Map([
+        ["low", 0.4],
+        ["medium", 0.3],
+        ["high", 0.2],
+    ]);
+    var roundaboutEfficiancy = 1;
+    var trafficLightefficiancy = 1;
+    var stopSignEfficiancy = 1;
+    //check for missing inputs
+    if (roads.length < 4) {
+        console.log("not enough roads entered as params");
+        return;
     }
-    return total;
+    //Function is designed to find out whats efficiant for the most number of cars
+    var testEfficiancy = function (map) {
+        var calcEfficiancy = 1;
+        roads.forEach(function (road) {
+            var _a, _b, _c;
+            if (road < 10) {
+                calcEfficiancy += road * ((_a = map.get("low")) !== null && _a !== void 0 ? _a : 0);
+            }
+            else if (road >= 10 && road < 20) {
+                calcEfficiancy += road * ((_b = map.get("medium")) !== null && _b !== void 0 ? _b : 0);
+            }
+            else {
+                calcEfficiancy += road * ((_c = map.get("high")) !== null && _c !== void 0 ? _c : 0);
+            }
+        });
+        console.log(calcEfficiancy);
+        return calcEfficiancy;
+    };
+    roundaboutEfficiancy = testEfficiancy(roundaboutMap);
+    stopSignEfficiancy = testEfficiancy(stopSignMap);
+    trafficLightefficiancy = testEfficiancy(trafficLightMap);
+    console.log("road values: ", roads[0], roads[1], roads[2], roads[3]);
+    if (roundaboutEfficiancy >= stopSignEfficiancy &&
+        roundaboutEfficiancy >= trafficLightefficiancy) {
+        console.log("A roundabout is the most efficiant solution");
+    }
+    else if (stopSignEfficiancy >= trafficLightefficiancy) {
+        console.log("A stop sign is the most efficiant solution");
+    }
+    else {
+        console.log("A traffic light system is the most efficiant solution");
+    }
+    console.log("roundabout eff: ", roundaboutEfficiancy);
+    console.log("traffic light eff: ", trafficLightefficiancy);
+    console.log("stop sign eff: ", stopSignEfficiancy);
+    return;
 }
-;
--- index.ts
+++ index.ts
@@ -1,35 +1,96 @@
-const myArgs = process.argv.slice(2);
+import * as fs from "fs";
+import { parse } from "csv-parse";
 
-console.log(romanToInt(myArgs[0]));
+const findTrafficSolutionFromCSV = (fileLocaiton: string) => {
+  var csvData: Array<Array<string>> = [];
+  fs.createReadStream(fileLocaiton)
+    .pipe(parse({ delimiter: ":" }))
+    .on("data", function (csvrow : Array<String>) {
+      csvData.push( csvrow[0].split(","));
+    })
+    .on("end", function () {
+      csvData.forEach((roadEntry) => {
+        const roads: Array<number> = roadEntry.map((numString) => {
+            return parseInt(numString, 10);
+          });
+          findTrafficSolution(roads);
+      })
+    });
 
-function romanToInt(s: string): number {
-    const romanMap = new Map<String, number>([
-        ['I' , 1,],
-        ['V' , 5,],
-        ['X' , 10,],
-        ['L' , 50,],
-        ['C' , 100,],
-        ['D' , 500,],
-        ['M' , 1000,],
-    ]);
-    
-    let total : number = 0;
-    let prevNum : number| null = null;
-    let toAdd : number | null = null;
-
-    
-    for(let i= 0; i< s.length; i++) {
-        let value = romanMap.get(s[i]);
-        if(value != null) {
-            if (prevNum != null && prevNum < value) {
-                total -= prevNum *2;
-            }
-            total += value;
-            prevNum = value;
-        }
-
-
-    }
-    
-    return total;
 };
+
+findTrafficSolutionFromCSV("./data.csv");
+
+const findTrafficSolutionWithArgs = () => {
+  const myArgs = process.argv.slice(2);
+  const roads: Array<number> = myArgs.map((numString) => {
+    return parseInt(numString, 10);
+  });
+  findTrafficSolution(roads);
+};
+
+function findTrafficSolution(roads: Array<number>) {
+  const roundaboutMap = new Map<string, number>([
+    ["low", 0.9],
+    ["medium", 0.75],
+    ["high", 0.5],
+  ]);
+  const trafficLightMap = new Map<string, number>([
+    ["low", 0.3],
+    ["medium", 0.75],
+    ["high", 0.9],
+  ]);
+  const stopSignMap = new Map<string, number>([
+    ["low", 0.4],
+    ["medium", 0.3],
+    ["high", 0.2],
+  ]);
+
+  let roundaboutEfficiancy = 1;
+  let trafficLightefficiancy = 1;
+  let stopSignEfficiancy = 1;
+
+  //check for missing inputs
+  if (roads.length < 4) {
+    console.log("not enough roads entered as params");
+    return;
+  }
+
+  //Function is designed to find out whats efficiant for the most number of cars
+  const testEfficiancy = (map: Map<string, number>) => {
+    let calcEfficiancy = 1;
+    roads.forEach((road: number) => {
+      if (road < 10) {
+        calcEfficiancy += road * (map.get("low") ?? 0);
+      } else if (road >= 10 && road < 20) {
+        calcEfficiancy += road * (map.get("medium") ?? 0);
+      } else {
+        calcEfficiancy += road * (map.get("high") ?? 0);
+      }
+    });
+    return calcEfficiancy;
+  };
+
+  roundaboutEfficiancy = testEfficiancy(roundaboutMap);
+  stopSignEfficiancy = testEfficiancy(stopSignMap);
+  trafficLightefficiancy = testEfficiancy(trafficLightMap);
+
+  console.log("road values: ", roads[0], roads[1], roads[2], roads[3]);
+
+  if (
+    roundaboutEfficiancy >= stopSignEfficiancy &&
+    roundaboutEfficiancy >= trafficLightefficiancy
+  ) {
+    console.log("A roundabout is the most efficiant solution");
+  } else if (stopSignEfficiancy >= trafficLightefficiancy) {
+    console.log("A stop sign is the most efficiant solution");
+  } else {
+    console.log("A traffic light system is the most efficiant solution");
+  }
+
+  console.log("roundabout eff: ", roundaboutEfficiancy);
+  console.log("traffic light eff: ", trafficLightefficiancy);
+  console.log("stop sign eff: ", stopSignEfficiancy);
+
+  return;
+}
